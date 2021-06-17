6647d2006450307526f71b5ba8247e370f967166
Josefine Lantz <jo.lantz@icloud.com>
2021-06-17 19:52:24 +0200

    implemented deposits

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,12 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "../../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./Token.sol";
+
 contract Exchange {
+    using SafeMath for uint256;
+
     address public feeAccount; // The account where fees go
     uint256 public feePercent; // Fee percentage
+    address constant ETHER = address(0); // Allows for storing Ether in tokens mapping.
+    // Token address, user who deposited address, user balance
+    mapping(address => mapping(address => uint256)) public tokens;
 
-    constructor(address _feeAccount, uint256 _feePercent) public {
+    // Events
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+
+    constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    receive() external payable {
+        revert();
+    }
+
+    function depositEther() public payable {
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+        emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+    }
+
+    // address to the actual token, in our case the address of token deploed in Token.sol.
+    function depositToken(address _token, uint256 _amount) public {
+        require(_token != ETHER);
+        // Send tokens to this contract, it could be any ERC20 token
+        // Instance of this token on the Ethereum network
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,22 +1,30 @@
 import { should } from "chai";
-import { tokens, EVM_REVERT } from "./helpers";
+import { tokens, ether, EVM_REVERT, ETHER_ADDRESS } from "./helpers";
 
+const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
 require("chai")
 	.use(require("chai-as-promised"))
 	.should()
 
-contract("Exchange", ([deployer, feeAccount]) => {
+contract("Exchange", ([deployer, feeAccount, user1]) => {
+	let token
 	let exchange 
 	const feePercent = 10;
 	let result
 
 	beforeEach(async () => {
+		// Deploy token
+		token = await Token.new();
+		// Deploy exchange
 		exchange = await Exchange.new(feeAccount, feePercent)
+		// Transfer tokens to user 1
+		token.transfer(user1, tokens(100), { from: deployer })
 	})
 
 	describe("deployment", () => {
+		let result
 		it("tracks the fee account", async () => {
 			result = await exchange.feeAccount() 
 			result.should.equal(feeAccount)
@@ -26,6 +34,82 @@ contract("Exchange", ([deployer, feeAccount]) => {
 			result = await exchange.feePercent() 
 			result.toString().should.equal(feePercent.toString())
 		})
+	})
+
+	describe("fallback", () => {
+		it("reverts when Ether is sent", async () => {
+			await exchange.sendTransaction({ value: 1, from: user1 }).should.be.rejectedWith(EVM_REVERT)
+		})
+	})
+
+	describe("depositing ether", () => {
+		let result
+		let amount
+
+		beforeEach(async () => {
+			amount = ether(1)
+			result = await exchange.depositEther({ from: user1, value: amount} )
+		})
+		
+		it("tracks the Ether deposit", async () => {
+			// Check the token balance
+			const balance = await exchange.tokens(ETHER_ADDRESS, user1)
+			balance.toString().should.equal(amount.toString())
+		})
+
+		it("emits a Deposit event", async () => {
+			const log = result.logs[0]
+			log.event.should.eq("Deposit")
+			const event = log.args
+			event.token.should.equal(ETHER_ADDRESS, "token address is correct")
+			event.user.should.equal(user1, "user address is correct")
+			event.amount.toString().should.equal(amount.toString(), "amount is correct")
+			event.balance.toString().should.equal(amount.toString(), "balance is correct")
+		})
+	})
+
+	describe("depositing tokens", () => {
+		let amount
+		let result
+		
+		describe("success", () => {
+			beforeEach(async () => {
+				amount = tokens(10)
+				await token.approve(exchange.address, amount, { from: user1} )
+				result = await exchange.depositToken(token.address, amount, { from: user1 })
+			})
+
+			it("tracks the token deposit", async () => {
+				// Check the token balance
+				let balance
+				balance = await token.balanceOf(exchange.address)
+				balance.toString().should.equal(amount.toString())
+				balance = await exchange.tokens(token.address, user1)
+				balance.toString().should.equal(amount.toString())
+			})
+
+			it("emits a Deposit event", async () => {
+				const log = result.logs[0]
+				log.event.should.eq("Deposit")
+				const event = log.args
+				event.token.should.equal(token.address, "token address is correct")
+				event.user.should.equal(user1, "user address is correct")
+				event.amount.toString().should.equal(amount.toString(), "amount is correct")
+				event.balance.toString().should.equal(amount.toString(), "balance is correct")
+			})
+	 	})
+
+		describe("failure", () => {
+			it("rejects Ether deposits", async() => {
+				await exchange.depositToken(ETHER_ADDRESS, tokens(10), { from: user1 }).should.be.rejected 
+			})
+			it("fails when no tokens are approved", async () => {
+				// Try depositing without approving before
+				await exchange.depositToken(token.address, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+			})
+			
+	 	})
+		
 	})
 })
 
