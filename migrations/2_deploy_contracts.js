f110bf69875d5ef55bb4b2d5b17e23631937d229
Josefine Lantz <jo.lantz@icloud.com>
2021-06-20 14:41:21 +0200

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,12 @@
 // Reading the compiled contract abi and creates a JS Object representation of the contract. 
 const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function (deployer) {
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts()
   deployer.deploy(Token);
+  const feeAccount = accounts[0]
+  const feePercent = 10
+  
+  deployer.deploy(Exchange, feeAccount, feePercent)
 };
